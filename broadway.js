9ebcfed38dcc587ea1d9c97aaeccc6c51fe68c76
driceduconge <drice.duconge@gmail.com>
2020-10-26 17:52:06 +0100

    #3 début travail

--- broadway.js
+++ broadway.js
@@ -1,3 +1,22 @@
+$(".couv").fadeOut(2000);
+$(".edito").click(function(){
+	$(".edito").fadeOut(2000);
+});
+var clicks = 0;
+$(".burger").click(function(){
+	if(clicks==0){
+		$(".menu-artistes").animate({
+			height:"100vh"
+		},500, function(){});
+		$(".bookmarks").slideToggle( "slow", function() {});
+	clicks = 1;}
+	else{
+	$(".menu-artistes").animate({
+		height:"50px"
+	},500, function(){});
+	$(".bookmarks").slideToggle( "slow", function() {});
+	clicks = 0;}
+});
 //J'ai rien touché à part le startX et startY, il faudra peut être bidouiller
 //pour faire des zooms ou pour arranger ça pr tel!
 var myScroll;
@@ -12,6 +31,7 @@ function loaded () {
 		mouseWheel: true,
 	});
 }
+
 
 // 	capture: false,
 // 	passive: false
