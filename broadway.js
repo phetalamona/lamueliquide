504333e0e4888cfb67af23b6cffe878ff7edc1a3
drice <drice.duconge@gmail.com>
2020-11-25 15:56:01 +0100

    Fix #10

--- broadway.js
+++ broadway.js
@@ -2,6 +2,7 @@ $(".couv").fadeOut(2000);
 $(".edito").click(function(){
 	$(".edito").fadeOut(2000);
 });
+
 var clicks = 0;
 $(".burger").click(function(){
 	if(clicks==0){
@@ -16,6 +17,43 @@ $(".burger").click(function(){
 	},500, function(){});
 	$(".bookmarks").slideToggle( "slow", function() {});
 	clicks = 0;}
+});
+
+function createDiv(w,h,x,y,d){
+	//permet de créer une div
+	var newDiv = document.createElement("div");
+	newDiv.style.position = "fixed";
+	newDiv.style.background = "var(--violet)";
+	newDiv.style.width = w+"px";
+	newDiv.style.height = h+"px";
+	newDiv.style.top = x+"px";
+	newDiv.style.left=  y+"px";
+	newDiv.className = "temp";
+	newDiv.innerHTML= d;
+	newDiv.style.color = "var(--ivoire)";
+	newDiv.style.fontSize = "30pt";
+	newDiv.style.textAlign = "center";
+	newDiv.style.fontFamily="'Redaction 50'";
+	newDiv.style.paddingTop = ((44*h)/100)+"px";
+	newDiv.style.paddingLeft = ((1*w)/100)+"px";
+	console.log(x);
+	document.querySelector(".scroller").appendChild(newDiv);
+}
+
+$(".image").click(function(){
+		var idimage = $(this).attr('id');
+		console.log(idimage);
+		var width = this.width;
+		var height = this.height;
+		var top = this.offsetTop;
+		var left = this.offsetLeft;
+		var metadata = this.dataset.meta;
+		console.log(width);
+		console.log(height);
+		console.log(top);
+		console.log(left);
+	  createDiv(width,height,top,left,metadata);
+		$(".temp").fadeOut(6000);
 });
 //J'ai rien touché à part le startX et startY, il faudra peut être bidouiller
 //pour faire des zooms ou pour arranger ça pr tel!
